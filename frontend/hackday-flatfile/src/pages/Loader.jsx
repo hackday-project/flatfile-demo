0c8836f5e7543bb368f70fcb1576bb1c2479f25f
Angus Leung <angusleung7@gmail.com>
2022-04-06 14:09:53 -0400

    Added upload UI components for loader flatfile frontend

--- frontend/hackday-flatfile/src/pages/Loader.jsx
+++ frontend/hackday-flatfile/src/pages/Loader.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar/Navbar';
+import CustomCard from '../components/CustomCard/CustomCard';
 
 import './Loader.css';
+import brand from '../assets/brand.webp';
+import category from '../assets/category.jpeg';
 
 const Loader = () => {
   return (
     <div className='loader-container'>
       <Navbar/>
+      <div className='cards-container'>
+        <CustomCard image={brand}/>
+        <CustomCard image={category}/>
+      </div>
     </div>
   )
 }
