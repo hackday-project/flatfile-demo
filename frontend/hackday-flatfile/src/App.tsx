5e46bd0f6683049c8f1e6e8954f5aae35bd259eb
Angus Leung <angusleung7@gmail.com>
2022-04-06 15:08:21 -0400

    Change project to typescript

--- frontend/hackday-flatfile/src/App.tsx
+++ frontend/hackday-flatfile/src/App.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import Loader from './pages/Loader';
 
-function App() {
+const App = () => {
   return (
     <>
       <Loader/>
@@ -8,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
--- frontend/hackday-flatfile/src/components/CustomCard/CustomCard.tsx
+++ frontend/hackday-flatfile/src/components/CustomCard/CustomCard.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useRef, useState } from "react";
+import PropTypes, { InferProps } from 'prop-types'
+
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+// import { FlatfileButton } from '@flatfile/react/src'
 
-const CustomCard = ({image}) => {
+
+const embedId = "897b2c8b-123e-428c-a51d-354b9b834426";
+const endUserEmail = "angusleung228@hotmail.com";
+const privateKey = "WV5ups3cIjAkgmp6PdZsHwDUXuCXXe5N9y9yiGGSvahQewRV1c0VJiTVI8L7H5YZ";
+
+const CustomCard: any = ({image}: InferProps<typeof CustomCard.propTypes>) => {
 
   return (
     <Card style={{ width: '18rem' }}>
@@ -17,5 +26,9 @@ const CustomCard = ({image}) => {
     </Card>
   )
 }
+
+CustomCard.propTypes = {
+  image: PropTypes.string
+};
 
 export default CustomCard
