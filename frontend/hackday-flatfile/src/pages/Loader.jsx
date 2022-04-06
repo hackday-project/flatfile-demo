525b0babc61bdcee125a438a8c0e25d34d6f7804
Angus-Leung <angusleung7@gmail.com>
2022-04-06 17:38:17 -0400

    fixed incorrect text (#5)

--- frontend/hackday-flatfile/src/pages/Loader.jsx
+++ frontend/hackday-flatfile/src/pages/Loader.jsx
@@ -7,12 +7,15 @@ import brand from '../assets/brand.webp';
 import category from '../assets/category.jpeg';
 
 const Loader = () => {
+  const brandDescription = "Upload a brand threshold CSV to Flatfile"
+  const categoryDescription = "Upload a category threshold CSV to Flatfile"
+
   return (
     <div className='loader-container'>
       <Navbar/>
       <div className='cards-container'>
-        <CustomCard image={brand}/>
-        <CustomCard image={category}/>
+        <CustomCard title="Brand Thresholds" description={brandDescription} image={brand}/>
+        <CustomCard title="Category Thresholds" description={categoryDescription} image={category}/>
       </div>
     </div>
   )
