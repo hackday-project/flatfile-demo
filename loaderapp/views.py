e32792675c83fc4dc66625a3c3865c83a10b548a
Lucia Lu <lucia.lu@REM-LAP-DEV17.hsd1.il.comcast.net>
2022-04-06 14:29:10 -0500

    well we have a working db that creates brand and threshold tables and also a url

--- loaderapp/views.py
+++ loaderapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def load_brand_file(request):
+    return HttpResponse('We loaded the brand file (not really)')
+
