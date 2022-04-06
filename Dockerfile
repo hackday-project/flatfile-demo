937c790dd8baf27b05b6fabe690b2e96745b59c0
Lucia Lu <lucia.lu@REM-LAP-DEV17.hsd1.il.comcast.net>
2022-04-06 13:30:59 -0500

    got a docker situation and starting a django situation

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.6.12-stretch AS base
+
+# set a directory for the app
+WORKDIR /flatfile-demo/
+
+# copy everything
+COPY . /flatfile-demo
+
+# install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# define the port number the container should expose
+EXPOSE 5000
+
+# set up db
+ RUN python3 manage.py migrate
---
+++ loaderapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Brand(models.Model):
+    key = models.CharField(max_length=64, unique=True)
+    name = models.CharField(max_length=64)
+
+
+class BrandThreshold(models.Model):
+    version = models.CharField(max_length=12)
+    brand = models.ForeignKey(Brand, to_field="key", on_delete=models.CASCADE)
+    min_threshold = models.DecimalField(max_digits=17, decimal_places=16)
+
+
