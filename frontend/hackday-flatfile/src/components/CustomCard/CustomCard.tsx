19c24cd34823ebd9accc57c86f98be65f028a62f
Angus Leung <angusleung7@gmail.com>
2022-04-07 12:57:34 -0400

    Added embed upload to frontend

--- frontend/hackday-flatfile/src/components/CustomCard/CustomCard.tsx
+++ frontend/hackday-flatfile/src/components/CustomCard/CustomCard.tsx
@@ -1,15 +1,29 @@
-import PropTypes, { InferProps } from 'prop-types'
+import PropTypes, { InferProps } from 'prop-types';
 
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-// import { FlatfileButton } from '@flatfile/react/src'
 
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+import { flatfileImporter } from "@flatfile/sdk";
 
 const embedId = "897b2c8b-123e-428c-a51d-354b9b834426";
 const endUserEmail = "angusleung228@hotmail.com";
 const privateKey = "WV5ups3cIjAkgmp6PdZsHwDUXuCXXe5N9y9yiGGSvahQewRV1c0VJiTVI8L7H5YZ";
 
 const CustomCard: any = ({ title, description, image}: InferProps<typeof CustomCard.propTypes>) => {
+
+  const openFlatfile = async (): Promise<any> => {
+    const importer = flatfileImporter("");
+
+    await importer.__unsafeGenerateToken({
+      privateKey: privateKey,
+      embedId: embedId,
+      endUserEmail: endUserEmail,
+    });
+
+
+    importer.launch();
+  }
 
   return (
     <Card style={{ width: '18rem' }}>
@@ -19,7 +33,33 @@ const CustomCard: any = ({ title, description, image}: InferProps<typeof CustomC
         <Card.Text>
           {description}
         </Card.Text>
-        <Button variant="primary">Upload</Button>
+        {/* <FlatfileButton
+          licenseKey="e4e520f6-3026-432a-8ca3-49418bada848"
+          customer={{
+              companyId: "ABC-123",
+              companyName: "ABC Corp.",
+              email: "john@abc123.com",
+              name: "John Smith",
+              userId: "12345"
+          }}
+          settings={{
+              type: "Brand Thresholds",
+              fields: [
+                  { label: "Version", key: "version" },
+                  { label: "Minimum Threshold", key: "min_threshold" },
+                  { label: "Brand Name", key: "key" }
+              ],
+              managed: true
+          }}
+          onData={async (results) => {
+              // Do something with the data here
+              console.log(results);
+              return "Done!";
+          }}
+      >
+        Upload
+      </FlatfileButton> */}
+        <Button variant="primary" onClick={() => openFlatfile()}>Upload</Button>
       </Card.Body>
     </Card>
   )
