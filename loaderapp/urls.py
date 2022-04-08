d38f1f125889a2ffe416b314762f7404e9e5aa83
Lucia Lu <lucia.lu@REM-LAP-DEV17.local>
2022-04-08 11:49:39 -0500

    jwt token

--- loaderapp/urls.py
+++ loaderapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path(r'trigger-brand', views.LoadBrandFile.as_view(), name='load_brand_file'),
-
+    path(r'embed-token', views.EmbedToken.as_view(), name='embed-token')
 ]
--- loaderapp/views.py
+++ loaderapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from django.http import HttpResponse, JsonResponse
 import requests
 import json
 from loaderapp.models import BrandThreshold, Brand
 from decimal import Decimal
 import traceback
+import jwt
 
 
 class FlatFileApi:
@@ -15,6 +15,7 @@ class FlatFileApi:
     FF_ACCESS_KEY = "FF00W5KYFFRV31OPMIC60W2OIZMBCJ4V4D28YN0O"
     FF_SECRET = "ziMyFUtBLHhciZxxMzHiu4BFOhp73nj9yPZeEf4v"
     AUTH_TOK = """eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFuZ3VzbGV1bmcyMjhAaG90bWFpbC5jb20iLCJzdWIiOiIyOTYyNyIsImFjY2Vzc0tleUlkIjoiRkYwMFc1S1lGRlJWMzFPUE1JQzYwVzJPSVpNQkNKNFY0RDI4WU4wTyIsImlhdCI6MTY0OTI3NDA5NCwiZXhwIjoxNjQ5Mjc0Njk0fQ.9AE4_gI3YBeICDjFApfXWwg9WFCbNocS9CMvJMmyZuw"""  # noqa
+
     headers = {
         "Content-Type": "application/json",
         "X-Api-Key": f"{FF_ACCESS_KEY}+{FF_SECRET}",
@@ -38,28 +39,37 @@ class FlatFileApi:
 
 class LoadBrandFile(APIView):
 
-    permission_classes = (IsAuthenticated,)
+    def post(self, request):
+        id = request.POST['batch_id']
+        dct = FlatFileApi().fetch_rows_by_batch_id(id)
+        errors = list()
+        successes = 0
+        for d in dct['data']:
+            try:
+                row = d['mapped']
+                version = row['version']
+                threshold = Decimal(row['min_threshold'])
+                brand, _ = Brand.objects.get_or_create(key=row['key'], defaults={'name': row['key']})
+                BrandThreshold.objects.create(version=version, min_threshold=threshold, brand=brand)
+            except Exception:
+                errorJson = dict(id=d['id'], error=traceback.format_exc())
+                errors.append(errorJson)
+            else:
+                successes += 1
+        response = {'batch_id': id, 'errors': errors, 'successes': f'{successes}/{dct["pagination"]["totalCount"]}'}
+        return JsonResponse(response, safe=False)
+
+
+class EmbedToken(APIView):
+    BRAND_EMBED_PRIVATE_KEY = "WV5ups3cIjAkgmp6PdZsHwDUXuCXXe5N9y9yiGGSvahQewRV1c0VJiTVI8L7H5YZ"
+    BRAND_EMBED_ID = "897b2c8b-123e-428c-a51d-354b9b834426"
 
     def post(self, request):
-        if request.method == 'GET':
-            return HttpResponse('We loaded the brand file (not really)')
-        if request.method == 'POST':
-            id = request.POST['batch_id']
-            dct = FlatFileApi().fetch_rows_by_batch_id(id)
-            errors = list()
-            successes = 0
-            for d in dct['data']:
-                try:
-                    row = d['mapped']
-                    version = row['version']
-                    threshold = Decimal(row['min_threshold'])
-                    brand, _ = Brand.objects.get_or_create(key=row['key'], defaults={'name': row['key']})
-                    BrandThreshold.objects.create(version=version, min_threshold=threshold, brand=brand)
-                except Exception:
-                    errorJson = dict(id=d['id'], error=traceback.format_exc())
-                    errors.append(errorJson)
-                else:
-                    successes += 1
-            response = {'batch_id': id, 'errors': errors, 'successes': f'{successes}/{dct["pagination"]["totalCount"]}'}
-            return JsonResponse(response, safe=False)
-
+        sub = request.POST['user_id']
+        load_type = request.POST['type']
+        if load_type == 'brand':
+            embed_id = self.BRAND_EMBED_ID
+            priv_key = self.BRAND_EMBED_PRIVATE_KEY
+        tok = jwt.encode({'embed': embed_id, 'sub': sub}, key=priv_key)
+        result = {'type': load_type, 'user_id': sub, 'token': tok}
+        return JsonResponse(result)
